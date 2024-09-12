30f00c23c31e6af5f68cf8216a1e13e689124943
Ayush Kumar Srivastava <ayushksrivastava29@gmail.com>
2024-09-12 23:26:12 +0530

    revert back without security

--- src/main/java/com/blog/blogging/config/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.blog.blogging.config;
-
-public class AppConstants {
-
-    public static final String PAGE_NUMBER = "0";
-    public static final String PAGE_SIZE = "10";
-    public static final String SORT_DIRECTION = "ASC";
-    public static final String SORT_BY = "postId";
-}
